38a75210dc963ace32741f80abc2a5c6991b2c92
princekr96 <princekr700@gmail.com>
2023-07-03 22:49:29 +0530

    added font

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,3 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
 import type { LinksFunction } from "@remix-run/node";
 import {
   Links,
@@ -20,6 +19,9 @@ export default function App() {
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital@1&family=Roboto:wght@100&family=Ubuntu:ital,wght@0,400;1,300&display=swap" rel="stylesheet" />
       </head>
       <body>
         <Outlet />
